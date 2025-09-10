51227d9577989082b75d4c36aa1305b8b67e2662
Natthajak D. <141382443+66070257@users.noreply.github.com>
2025-09-10 22:21:17 +0700

    Update authentication

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,31 +1,28 @@
-import jwt from "jsonwebtoken"
-import userService from "../services/userService.js"
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
 
 const authMiddleware = () => {
-    return async (req, res, next) => {
-        try {
-            const jwt_secret = process.env.JWT_SECRET
-            const authHeader = req.headers["authorization"]
+  return async (req, res, next) => {
+    try {
+      const jwt_secret = process.env.JWT_SECRET || "secret";
+      const authHeader = req.headers["authorization"];
 
-            if (!authHeader || !authHeader.startsWith("Bearer ")) {
-                return res.status(401).json({ message: "No token provided" })
-            }
+      if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "No token provided" });
+      }
 
-            const token = authHeader.split(" ")[1]
-            const decodedToken = jwt.verify(token, jwt_secret)
+      const token = authHeader.split(" ")[1];
+      const decodedToken = jwt.verify(token, jwt_secret);
 
-            const user = await userService.getUserById(decodedToken.userId)
-            if (!user) {
-                return res.status(401).json({ message: "Invalid token" })
-            }
+      const user = await User.findById(decodedToken.id);
+      if (!user) return res.status(401).json({ message: "Invalid token" });
 
-            // เก็บทั้ง user object (ยืดหยุ่นกว่า)
-            req.user = user
-            next()
-        } catch (err) {
-            return res.status(401).json({ message: "Unauthorized" })
-        }
+      req.user = user;
+      next();
+    } catch (err) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
-}
+  };
+};
 
-export default authMiddleware
+export default authMiddleware;
